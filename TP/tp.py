984d2bf4ef200f99f5c02c1ccd7c5c8a7e9fdc73
martinmateos2001 <martinmateos_2001@hotmail.com>
2025-10-13 15:01:47 -0300

    intento hacer dataframes

--- TP/tp.py
+++ TP/tp.py
@@ -1,3 +1,9 @@
 import numpy as np
 import pandas as pd
 import duckdb as dd
+#%% limpieza de codigo
+df_ee_padron = pd.read_excel('TP/2022_padron_oficial_establecimientos_educativos.xlsx')
+df_ep_datos = pd.read_csv('TP/Datos_por_departamento_actividad_y_sexo.csv')
+
+print(df_ee_padron.head())
+# %%
