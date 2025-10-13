79587f3c9f7a8baad23d4b0fcc1ddb76f9953338
martinmateos2001 <martinmateos_2001@hotmail.com>
2025-10-13 15:56:53 -0300

    Filtro las columnas que voy a usar del padron de establecimientos esducativos

--- TP/tp.py
+++ TP/tp.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import duckdb as dd
 #%% limpieza de codigo
-df_ee_padron = pd.read_excel(sheet_name='TP/2022_padron_oficial_establecimientos_educativos.xlsx')
-df_ep_datos = pd.read_csv('TP/Datos_por_departamento_actividad_y_sexo.csv')
+columnas_ee = 'A:C,G,L,N,V:Y'
 
-df_ee_padron.head()
-# %%
+df_ee_padron = pd.read_excel('TP\\2022_padron_oficial_establecimientos_educativos.xlsx', sheet_name='padron2022', skiprows=6, usecols=columnas_ee)
+df_ep_datos = pd.read_csv('TP\Datos_por_departamento_actividad_y_sexo.csv')
+
+print(df_ee_padron.head())
+#%%
